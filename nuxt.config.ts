20a12330312ebb70dd1fb6db684ae1659b54a52c
André Silva de Oliveira <andreoliveira@MacBook-Pro-de-Andre.local>
2025-02-27 13:30:21 -0300

    adjustment in google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,28 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      link: [{ rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' }]
+      link: [{ rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' }],
+      script: [
+        {
+          hid: 'google-analytics',
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-P6HJWKZ279',
+          async: true,
+        },
+        {
+          hid: 'google-analytics-config',
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-P6HJWKZ279');
+          `,
+          type: 'text/javascript',
+          charset: 'utf-8',
+        }
+      ],
+      __dangerouslyDisableSanitizersByTagID: {
+        'google-analytics-config': ['innerHTML'],
+      },
     }
   },
   compatibilityDate: '2024-11-01',
@@ -11,25 +32,15 @@ export default defineNuxtConfig({
 
   css: ['~/assets/scss/main.scss'],
 
-  modules: [
-    ['@nuxtjs/google-fonts', {
-      families: {
-        Labrada: {
-          wght: '300..400',
-          ital: '400',
-        },
-        Inter: [300, 400],
-      }
-    }], 
-    '@nuxt/image',
-    '@nuxtjs/google-gtag'
-  ],
-
-  runtimeConfig: {
-    public: {
-      googleAnalyticsId: 'G-P6HJWKZ279',
+  modules: [['@nuxtjs/google-fonts', {
+    families: {
+      Labrada: {
+        wght: '300..400',
+        ital: '400',
+      },
+      Inter: [300, 400],
     }
-  },
+  }], '@nuxt/image'],
 
   devServer: {     
     port: 3000, // default: 3000     
